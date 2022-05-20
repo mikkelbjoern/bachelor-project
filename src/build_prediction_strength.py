31f24c6e7386d4a38f148838f3a73beac246ad73
Mikkel Goldschmidt <m@goldsch.dk>
2022-05-20 12:48:34 +0200

    🔎 Showed that performance on ruler imgs are worse

--- src/build_prediction_strength.py
+++ src/build_prediction_strength.py
@@ -31,9 +31,6 @@ def build_prediction_strength():
 
     predictions["correct"] = predictions.classification == predictions.real_class
 
-    # Print the precision
-    print(np.mean(predictions["correct"]))
-
     # Make a confusion matrix
     confusion_matrix = (
         predictions.groupby(["real_class", "classification"]).size().unstack()
@@ -66,7 +63,6 @@ def build_prediction_strength():
         predictions["real_class"] == short_to_full_name_dict["mel"]
     ]
     mel_confusion_matrix = only_melanoma.groupby(["ruler", "correct"]).size().unstack()
-    print(type(mel_confusion_matrix))
     # Heatmap of correct melanoma predictions sepearted on ruler vs. non-ruler
     plt.figure(figsize=TINY_SIZE)
     p = sns.heatmap(
@@ -78,7 +74,6 @@ def build_prediction_strength():
 
     # Make a chi-square test on the mel_confusion_matrix
     chi2, prop, dof, expected = stats.chi2_contingency(mel_confusion_matrix)
-    print(prop)
     with open("p_mel.txt", "w") as f:
         f.write(f"{round(float(prop), 3)}")
 
@@ -94,6 +89,39 @@ def build_prediction_strength():
     p.set_ylabel("Has ruler")
     p.set_xlabel("Correctly classified")
     plt.savefig("mel_confusion_matrix_seaborn_normalized.png")
+
+
+
+    #### Comparison of performance on cases with and without a ruler ####
+    plt.clf()
+
+    ruler_confusion_matrix = predictions.groupby(["ruler", "correct"]).size().unstack()
+    ruler_plot = sns.heatmap(
+        ruler_confusion_matrix, annot=True, cmap="Blues", fmt="d"
+    )
+    ruler_plot.set_ylabel("Has ruler")
+    ruler_plot.set_xlabel("Correctly classified")
+    plt.savefig("ruler_confusion_matrix_seaborn.png")
+
+
+
+    # Normalize over the x axis
+    plt.clf()
+    ruler_confusion_matrix_normalized = ruler_confusion_matrix.div(
+        ruler_confusion_matrix.sum(axis=1), axis=0
+    )
+    ruler_plot_normalized = sns.heatmap(
+        ruler_confusion_matrix_normalized, annot=True, cmap="Blues"
+    )
+    ruler_plot_normalized.set_ylabel("Has ruler")
+    ruler_plot_normalized.set_xlabel("Correctly classified")
+    plt.savefig("ruler_confusion_matrix_seaborn_normalized.png")
+
+    # Make a chi-square test on the ruler_confusion_matrix
+    chi2, prop, dof, expected = stats.chi2_contingency(ruler_confusion_matrix)
+    with open("p_ruler.txt", "w") as f:
+        f.write(f"{round(float(prop), 3)}")
+
 
 
 
